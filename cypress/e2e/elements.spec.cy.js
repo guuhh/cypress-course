956cf833e58af4f50639c015d6eaa2e8796fcd48
Gustavo Rodrigues <gustavo-rodrigues88@hotmail.com>
2023-02-16 16:10:09 -0300

    Adds it test to textarea and commands

--- cypress/e2e/elements.spec.cy.js
+++ cypress/e2e/elements.spec.cy.js
@@ -37,6 +37,34 @@ describe('Work with basic elements', () =>{
 
 
     })
+    it.only('TextFields', ()=>{
+        cy.get('input#formNome').type('Cypress test')
+        cy.get('input#formNome').should('have.value','Cypress test')
+
+
+        cy.get('textarea#elementosForm\\:sugestoes')
+        .type('textarea')
+        .should('have.value', 'textarea')
+
+        cy.get('#tabelaUsuarios > tbody > tr:nth-child(1) > td:nth-child(6) > input[type=text]')
+        .type('????')
+
+        cy.get('[data-cy=dataSobrenome]')
+        .type('Test12345{backspace}{backspace}')
+        .should('have.value', 'Test123')
+
+       /* Available sequences are: 
+       selectAll, moveToStart, moveToEnd, del, backspace, esc, enter, 
+       rightArrow, leftArrow, upArrow, downArrow, 
+       home, end, insert, pageUp, pageDown, 
+       {, alt, option, ctrl, control, meta, 
+       command, cmd, shift */
+
+        cy.get('textarea#elementosForm\\:sugestoes')
+        .clear()
+        .type('Erro{selectall}Acerto', {delay:100})
+        .should('have.value', 'Acerto')
+    })
 })
 
 //A new describe structure does not inherit "before" and "beforeEach" from another one.
